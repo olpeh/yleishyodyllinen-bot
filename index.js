e9e46b57d879a8acad1ef5ff745e137480eaa663
Olavi Haapala <olavi.haapala@futurice.com>
2019-01-17 20:13:54 +0200

    Don't wrap in function

--- index.js
+++ index.js
@@ -2,43 +2,40 @@ const Telegraf = require('telegraf');
 const Extra = require('telegraf/extra');
 require('dotenv').config();
 
-const setup = () => {
-  const botToken = process.env.BOT_TOKEN;
-  console.log('Setting up telegram bot...');
-  if (botToken) {
-    const bot = new Telegraf(botToken);
+const botToken = process.env.BOT_TOKEN;
+console.log('Setting up telegram bot...');
+if (!botToken) {
+  console.warn('Telegram bot token was missing... failed to start.');
+  return -1;
+}
 
-    bot.use((ctx, next) => {
-      const start = new Date();
-      return next(ctx).then(() => {
-        const ms = new Date() - start;
-        console.log('Response time %sms', ms);
-      });
-    });
+const bot = new Telegraf(botToken);
 
-    bot.start(ctx =>
-      ctx.reply(
-        'Terve. Olen Yleishyödyllinen botti. Lähetä komento */help* nähdäksesi mitä kaikkea osaan tehdä.',
-        Extra.markdown()
-      )
-    );
+bot.use((ctx, next) => {
+  const start = new Date();
+  return next(ctx).then(() => {
+    const ms = new Date() - start;
+    console.log('Response time %sms', ms);
+  });
+});
 
-    bot.command('help', ctx => {
-      console.log({ ctx });
-      ctx.reply(
-        `Tässäpä mitä osaan:
-        - /help -> Kerron mitä osaan
-        - muuta en vielä osaa 😢
-        `,
-        Extra.markdown()
-      );
-    });
+bot.start(ctx =>
+  ctx.reply(
+    'Terve. Olen Yleishyödyllinen botti. Lähetä komento */help* nähdäksesi mitä kaikkea osaan tehdä.',
+    Extra.markdown()
+  )
+);
 
-    bot.launch();
-    console.log('Bot listening to messages and commands...');
-  } else {
-    console.warn('Telegram bot token was missing... failed to start.');
-  }
-};
+bot.command('help', ctx => {
+  console.log({ ctx });
+  ctx.reply(
+    `Tässäpä mitä osaan:
+    - /help -> Kerron mitä osaan
+    - muuta en vielä osaa 😢
+    `,
+    Extra.markdown()
+  );
+});
 
-setup();
+bot.launch();
+console.log('Bot listening to messages and commands...');
